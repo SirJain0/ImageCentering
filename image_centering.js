a41de4d3fe1d2d653b6fcb63be4947bd3b843191
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-15 10:22:59 -0500

    Update quick message duration

--- image_centering.js
+++ image_centering.js
@@ -48,7 +48,7 @@
 	function center() {
 		setViewport();
 		setZoom();			
-		Blockbench.showQuickMessage("Centered viewport!", 2000);
+		Blockbench.showQuickMessage("Centered viewport!", 1500);
 	}
 
 	// Resets the viewport position
